49bf79a9d4a5d46e3202fa53e8548aa7fd817b08
bogaert <bogaert.david21@gmail.com>
2021-04-20 10:49:58 +0200

    menu in progress

--- menu.js
+++ menu.js
@@ -1,54 +1,35 @@
+new Vue({
+  el: '#food-menu',
+  data() {
+    return {
+     plates: [
+       {name: 'Spaghetti', price: '8.90 €'},
+       {name: 'Lasagne', price: '11.90 €'},
+        {name: 'Seafood paella', price: '13.90 €'},
+        {name: 'Chicken rice', price: '7.90 €'},
+        {name: 'Baked potates with bacon', price: '7.90 €'},
+        {name: 'Fish and chips', price: '8.90 €'},
+        {name: 'Poutine', price: '4.90 €'},
+        {name: 'Hamburger with chips', price: '9.90 €'},
 
-  const buttons = document.querySelectorAll('.button');
-  const menus = document.querySelectorAll('.menu');
-  
-  const highlight = document.createElement('span');
-  document.body.appendChild(highlight);
-  highlight.classList.add('highlight');
-  
-  // Set initial dimensions and position of 'highlight' based on activeButton coords 
-  function initialHightlightLocation() {
-    const activeButton = document.querySelector('.button--is-active');
-    const activeButtonCoords = activeButton.getBoundingClientRect();
-  
-    const initialCoords = {
-      width: activeButtonCoords.width,
-      height: activeButtonCoords.height,
-      left: activeButtonCoords.left + window.scrollX,
-      top: activeButtonCoords.top + window.scrollY
+     ],
+      drinks:[
+      {name: 'Water 0.5l', price: '2.50 €'},
+      {name: 'Small beer', price: '2.90 €'},
+      {name: 'Large beer', price: '3.90 €'},
+      {name: 'Mojito', price: '5.90 €'},
+      {name: 'Tequilla Sunrise', price: '7.90 €'},
+        ],
+        deserts: [
+       {name: 'Musse', price: '4.90 €'},
+       {name: 'Apple cake', price: '3.90 €'},
+      {name: 'Apple cake', price: '3.90 €'},
+       {name: 'Carrot cake', price: '6.90 €'},
+       {name: 'Tim tam cake', price: '9.90 €'},
+
+     ],
+      
     }
-  
-    highlight.style.width = `${initialCoords.width}px`;
-    highlight.style.height = `${initialCoords.height}px`;
-    highlight.style.transform = `translate(${initialCoords.left}px, ${initialCoords.top}px)`;
-  }
-  
-  function handleClick(e) {
-    e.preventDefault();
-  
-    buttons.forEach(button => button.classList.remove('button--is-active'));
-    this.classList.add('button--is-active');
-  
-    // Set current dimensions and position of 'highlight' based on the clicked button 
-    const buttonCoords = this.getBoundingClientRect();
-    const coords = {
-      width: buttonCoords.width,
-      height: buttonCoords.height,
-      left: buttonCoords.left + window.scrollX,
-      top: buttonCoords.top + window.scrollY
-    }
-    highlight.style.width = `${coords.width}px`;
-    highlight.style.height = `${coords.height}px`;
-    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
-  
-    // Show the menu associated to the clicked button
-    const targetMenu = document.querySelector(`#${this.dataset.target}`);
-    menus.forEach(menu => {
-      menu.classList.remove('menu--is-visible');
-      targetMenu.classList.add('menu--is-visible');
-    })
-  }
-  
-  window.addEventListener('load', initialHightlightLocation);
-  window.addEventListener('resize', initialHightlightLocation);
-  buttons.forEach(button => button.addEventListener('click', handleClick));
+  },
+ 
+})
